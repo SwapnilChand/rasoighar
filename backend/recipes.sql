afcfc480c86f7cfde9484ee42348a77fa7069b7b
Swapnil <swapnilchand51@gmail.com>
2025-07-07 11:06:31 +0530

    add frontend boilerplate

--- backend/recipes.sql
+++ backend/recipes.sql
@@ -1,9 +1,9 @@
 CREATE TABLE recipes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT,
-    ingredients TEXT,
-    steps TEXT,
-    category TEXT,
+    id TEXT PRIMARY KEY,
+    title TEXT NOT NULL,
+    ingredients TEXT NOT NULL,
+    steps TEXT NOT NULL,
+    category TEXT NOT NULL,
     image_url TEXT,
     is_tried INTEGER DEFAULT 0
 );
